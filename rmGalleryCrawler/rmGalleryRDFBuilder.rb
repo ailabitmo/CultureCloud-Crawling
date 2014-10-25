99ee8b4e6cad5f7fc1b253b02fee256b08e7f423
Alexey Andreyev <yetanotherandreyev@gmail.com>
2014-10-25 05:47:56 +0400

    switched to turtle format, data saving option added, prefixes added..

--- rmGalleryCrawler/rmGalleryRDFBuilder.rb
+++ rmGalleryCrawler/rmGalleryRDFBuilder.rb
@@ -1,67 +1,42 @@
-# rmgallery.ru HTML data parser
+# rmgallery.ru HTML data parser generated
+# rmgallery_art.xml to turtle-rdf data parser
+# with DBPedia enrichment
 # Alexey Andreyev: yetanotherandreyev@gmail.com
 
-=begin
-This software is licensed under the "Anyone But Richard M Stallman"
-(ABRMS) license, described below. No other licenses may apply.
-
-
---------------------------------------------
-The "Anyone But Richard M Stallman" license
---------------------------------------------
-
-Do anything you want with this program, with the exceptions listed
-below under "EXCEPTIONS".
-
-THIS SOFTWARE IS PROVIDED "AS IS" WITH NO WARRANTY OF ANY KIND.
-
-In the unlikely event that you happen to make a zillion bucks off of
-this, then good for you; consider buying a homeless person a meal.
-
-
-EXCEPTIONS
-----------
-
-Richard M Stallman (the guy behind GNU, etc.) may not make use of or
-redistribute this program or any of its derivatives.
-
-P.S.: just messing :)
-=end
-
-
 require 'rubygems'
-require 'colorize'
+
+require 'net/http'
 require 'uri'
 require 'json'
-require 'net/http'
-require 'nokogiri'
-require 'rdf/rdfxml'
+
+require 'pathname'
+require 'colorize'
 require 'io/console'
+
+require 'nokogiri'
+require 'rdf/turtle'
+
 
 # TODO: cotribute to dbpediafinder: https://github.com/moustaki/dbpediafinder/
 # TODO: images rdf
 # TODO: genres rdf
 # TODO: types rdf
 
-@proxy = URI.parse(ENV['HTTP_PROXY']) if ENV['HTTP_PROXY']
-
+# Get final Url After Redirects
 def getFinalUrl(url)
-  u = URI.parse(url)
-  h = Net::HTTP.new u.host, u.port
-
-  head = h.start do |ua|
-    ua.head u.path
-  end
-  
-  return head['location']
-  
+  return Net::HTTP.get_response(URI(url))['location']
 end
 
+# transform wikipedia link to dbpedia resource link
 def wikipediaToDBbpedia(wikipedia)
+  if ((wikipedia.nil?) or (wikipedia.empty?)) then return nil end
+  #TODO: check string with regexp
   url_key = wikipedia.split('/').last
   return "http://dbpedia.org/resource/" + url_key
 end
 
+# Search for wikipedia article links
+# by title with wikipedia search api
 def wikipediaSearch(label, locale="en")
   hostUrl = "http://#{locale}.wikipedia.org/"
   wikipedia_url_s = "#{hostUrl}w/api.php?action=query&format=json&list=search&srsearch=#{URI.encode(label)}&srprop="
@@ -71,14 +46,33 @@ def wikipediaSearch(label, locale="en")
   else
     h = Net::HTTP.new(url.host, url.port)
   end
+  h.open_timeout = 1
+  h.read_timeout = 1
   h.start do |h|
-    res = h.get(url.path + "?" + url.query)
+    begin
+      res = h.get(url.path + "?" + url.query)
+    rescue Exception => e
+      puts "#{e.message}. Press return to retry."
+      gets
+      retry
+    end
     json = JSON.parse(res.body)
-    results = json["query"]["search"].map { |result| getFinalUrl(URI.encode(hostUrl+"wiki/"+result["title"])) }
+    results = json["query"]["search"].map { |result|
+      getFinalUrl(URI.encode(hostUrl+"wiki/"+result["title"]))
+    }
     if (results.empty?) then
       suggestion = json["query"]["searchinfo"]["suggestion"]
-      if !(suggestion.empty?) then
+      if !(suggestion.nil?) then
 	return wikipediaSearch(suggestion,locale)
+      else
+	puts "Result for not found. Enter new search string or return to skip"
+	answer = gets
+        if (answer=="\n") then
+          puts "skipped"
+	  return nil
+	else 
+	  wikipediaSearch(answer,locale)
+	end
       end
     else
       return results
@@ -86,10 +80,15 @@ def wikipediaSearch(label, locale="en")
   end
 end
 
-def getDPediaName(searchString, disambiguation="")
-  puts "Searching for: #{searchString}...".blue.on_red.blink
+# Search for dbpedia resource link by title
+def getDPediaUrl(searchString, locale)
+  puts "Searching for: #{searchString} in #{locale}-wiki...".blue.on_red
   puts
-  foundWikiData = wikipediaSearch(searchString)
+  foundWikiData = wikipediaSearch(searchString, locale)
+  puts "Search results:"
+  
+  if foundWikiData.nil? then return end
+
   foundWikiDataSize = foundWikiData.size
   foundWikiDataSize.size.times { |i|                           
     puts "#{(i+1).to_s}.: #{foundWikiData[i]}".colorize(:color => (i==0)?:yellow : :light_yellow)             
@@ -98,8 +97,8 @@ def getDPediaName(searchString, disambiguation="")
   puts "Is first result ok? Enter:"
   puts "Return to accept first current result"
   puts "Number of wikipedia result (will be transformed to DBPedia resource)"
-  puts "New search string to specify it"
-  puts "- symbol to return empty string"
+  puts "New search string to specify and search again"
+  puts "- symbol to skip current artist"
   answer = gets
   answerToI = answer.to_i
   if (answer=="\n") then
@@ -109,43 +108,89 @@ def getDPediaName(searchString, disambiguation="")
   elsif ((answerToI!=0) and (answerToI<foundWikiDataSize))
     return wikipediaToDBbpedia(foundWikiData[answerToI-1])
   elsif (answer=="-\n")
-    return ""
+    return 
   else
-    getDPediaName(answer)
+    getDPediaUrl(answer,locale)
   end
 end
 
-artFile = File.open("rmgallery_art.xml","r")
+# DBPedia Spotlight text annotator
+# https://github.com/dbpedia-spotlight/dbpedia-spotlight/wiki
+def dpdepiaSpotlighAnnotator(inputText)
+  u = URI.encode("http://spotlight.dbpedia.org/rest/annotate?text=#{inputText}")
+  uri = URI.parse(u)
+  res = Net::HTTP.post_form(uri,'confidence' => @dbPediaSpotlightConfidence, 'support' => @dbPediaSpotlightSupport)
+  res[:Accept] = "text/xml"
+  puts res.body
+  return res.body
+end
 
-doc = Nokogiri::XML(artFile)
+
+# Authors rdf generator:
+
+@proxy = URI.parse(ENV['HTTP_PROXY']) if ENV['HTTP_PROXY']
+
+#TODO: ask for this values:
+@dbPediaSpotlightConfidence = 0.2
+@dbPediaSpotlightSupport = 20
 
 consoleWidth = IO.console.winsize[1]
+
+# source file #TODO: specify it
+artFile = File.open("rmgallery_art.xml","r")
+doc = Nokogiri::XML(artFile)
 
 authors = doc.xpath('//section[@label="author"]/sectionItem/fullName')
 authorsSize = authors.size
 puts "Found #{authors.size} authors"
-puts authors[0].parent.parent.parent["locale"]
 
-authorsGraph =  RDF::Graph.new
-
+# cidocCRM. TODO: choose version
 cidocCRM = RDF::Vocabulary.new('http://www.cidoc-crm.org/rdfs/cidoc_crm_v5.1-draft-2014March.rdfs#')
-puts cidocCRM
+rdf_prefixes = {
+  'cidoc-crm' =>  "http://www.cidoc-crm.org/rdfs/cidoc_crm_v5.1-draft-2014March.rdfs#",
+  rdf:  "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+  dbpedia:  "http://dbpedia.org/resource/"
+}
+
+# authors file # TODO: specify it
+rmgallery_authors_filepath = "rmgallery_authors.ttl"
+if !(File.file?(rmgallery_authors_filepath)) then
+  puts "#{rmgallery_authors_filepath} was not found in working dir"
+else
+  puts "#{rmgallery_authors_filepath} was found in working dir"
+  authorsFile = File.open(rmgallery_authors_filepath,"r")
+  previousAuthorsGraph = RDF::Graph.load(authorsFile) # FIXME: check is data ok
+  authorsFile.close
+end
+
+authorsGraph = RDF::Graph.new(:format => :ttl, :prefixes => rdf_prefixes)
+
+if !(previousAuthorsGraph.nil?) then
+  authorsGraph = previousAuthorsGraph
+end
 
 authorsSize.times { |i|
-  
-          
-  dbPediaResUrl = getDPediaName(authors[i].text, authors[i].parent.parent.parent["locale"])
-  authorURI = RDF::URI.new(dbPediaResUrl)
-  
-  authorsGraph << [authorURI, cidocCRM[:P2_has_type], cidocCRM.E21_Person]
-  authorsGraph << [authorURI, cidocCRM[:P1_is_identified_by], authors[i].text]
-  authorsGraph << [authorURI, cidocCRM[:P3_has_note], authors[i].parent.css("bio")[0].text]      
+  currentLocale = authors[i].parent.parent.parent["locale"] # FIXME: shame on me                  
+  authorFullName = authors[i].text 
+  authorFullNameLiteral = RDF::Literal.new(authorFullName, :language => currentLocale)                  
+  if (authorsGraph.query([nil, cidocCRM[:P1_is_identified_by], authorFullNameLiteral]).empty?) then
+    dbPediaResUrl = getDPediaUrl(authors[i].text, currentLocale)
+    authorURI = RDF::URI.new(dbPediaResUrl)          
+    authorsGraph.insert([authorURI, cidocCRM[:P2_has_type], cidocCRM.E21_Person])
+                
+    authorsGraph << [authorURI, cidocCRM[:P1_is_identified_by], authorFullNameLiteral]
+    annotationText = authors[i].parent.css("bio")[0].text     
+    authorsGraph << [authorURI, cidocCRM[:P3_has_note], annotationText]
+    # TODO: dpdepiaSpotlighAnnotator          
+    # dpdepiaSpotlighAnnotator(annotationText)                
+  end
+                
                   
-  percentage = "authors process: #{i+1} of #{authorsSize} "                
+  percentage = "authors processed: #{i+1} of #{authorsSize} "                
   puts percentage+"#"*(consoleWidth-percentage.length)
                   
-  authorsFile = File.new("rmgallery_authors.rdf","w")      
-  authorsFile.write(authorsGraph.dump(:rdfxml))
+  authorsFile = File.new("rmgallery_authors.ttl","w")      
+  authorsFile.write(authorsGraph.dump(:ttl, :prefixes => rdf_prefixes))
   authorsFile.close      
 }
 
