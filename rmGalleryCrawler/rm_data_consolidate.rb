8f8406e3e82e6f1a7568d5cff6df19c19cae33f6
oscii <iam@oscii.ru>
2014-12-03 12:33:14 +0300

    [+] rm_data_consolidate write to file

--- rmGalleryCrawler/rm_data_consolidate.rb
+++ rmGalleryCrawler/rm_data_consolidate.rb
@@ -7,6 +7,15 @@ require 'digest/md5'
 require 'russian'
 include RDF
 
+@rdf_prefixes = {
+    :ecrm =>  "http://erlangen-crm.org/current/",
+    :rdf => RDF.to_uri,
+    :rdfs => RDFS.to_uri,
+    :dbp =>  "http://dbpedia.org/resource/",
+    :owl => OWL.to_uri,
+    'rm-lod' => "http://rm-lod.org/"
+}
+
 @graph = RDF::Graph.new
 
 puts 'Loading'
@@ -14,8 +23,8 @@ puts 'dates'
 @graph.load('rm_artwork_dates.ttl')
 puts 'images'
 @graph.load('rm_artwork_images.ttl')
-puts 'materials'
-@graph.load('rm_artwork_materials.ttl')
+# puts 'materials'
+# @graph.load('rm_artwork_materials.ttl')
 puts 'objects'
 @graph.load('rm_artwork_objects.ttl')
 puts 'ownerships'
@@ -30,3 +39,10 @@ puts 'genres'
 @graph.load('rm_genres.ttl')
 puts 'persons'
 @graph.load('rm_persons.ttl')
+
+
+puts '== Writing file =='
+file = File.new('rm_data.ttl', 'w')
+file.write(@graph.dump(:ttl, :prefixes => @rdf_prefixes))
+file.close
+puts 'Done!'
