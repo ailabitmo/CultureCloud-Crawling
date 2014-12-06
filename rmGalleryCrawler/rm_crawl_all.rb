bdfe9f2a40047a23cc5b2e0a20e23389267634a4
Alexey Andreyev <yetanotherandreyev@gmail.com>
2014-12-06 18:39:49 +0300

    cleaning main script, updating ttl files

--- rmGalleryCrawler/rm_crawl_all.rb
+++ rmGalleryCrawler/rm_crawl_all.rb
@@ -1,16 +1,17 @@
-puts '== Starting big script =='
+puts '== Starting main big script =='
 
-puts '\nrm_crawl_artwork_ownerships\n'
+puts "\nrm_crawl_artwork_ownerships\n"
 require './rm_crawl_artwork_ownerships.rb'
 
-puts '\nrm_crawl_genres\n'
+puts "\nrm_crawl_genres\n"
 require './rm_crawl_genres.rb'
 
-puts 'rm_crawl_authors\n'
+puts "rm_crawl_authors\n"
 require './rm_crawl_authors.rb'
 
-puts '\nrm_crawl_images\n'
-require './rm_crawl_images.rb'
+# we already crawled all the images from rmgallery
+# puts "\nrm_crawl_images\n"
+# require './rm_crawl_images.rb'
 
-puts '\nrm_crawl_artworks\n'
+puts "\nrm_crawl_artworks\n"
 require './rm_crawl_artworks.rb'
